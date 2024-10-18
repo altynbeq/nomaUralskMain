cb564cc61a66d407912ec196f4cb7192c5c5d4de
Gennady <unclebens313@gmail.com>
2024-10-18 15:09:33 +0300

    Login fix

--- src/methods/dataFetches/getLeadsBack.js
+++ src/methods/dataFetches/getLeadsBack.js
@@ -1,5 +1,5 @@
-export async function getLeadsBack() {
-  const url = "https://nomalytica-back.onrender.com/api/users/67042877bf2a6d9b31acfe1e";
+export async function getLeadsBack(id) {
+  const url = `https://nomalytica-back.onrender.com/api/users/${id}`;
 
   try {
     const response = await fetch(url);
