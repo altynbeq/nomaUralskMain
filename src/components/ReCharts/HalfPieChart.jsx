a0a8f54c154eca8ccf97b30d90e307335a58fcc0
Altynbek Kuat <65371350+altynbeq@users.noreply.github.com>
2024-11-06 20:25:23 +0100

    Update HalfPieChart.jsx

--- src/components/ReCharts/HalfPieChart.jsx
+++ src/components/ReCharts/HalfPieChart.jsx
@@ -1,19 +1,12 @@
 import React, { PureComponent } from 'react';
 import { PieChart, Tooltip, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
 
-const data = [
-  { name: 'Group A', value: 400 },
-  { name: 'Group B', value: 300 },
-  { name: 'Group C', value: 300 },
-  { name: 'Group D', value: 200 },
-  { name: 'Group E', value: 278 },
-  { name: 'Group F', value: 189 },
-];
 
 export default class HalfPie extends PureComponent {
   static demoUrl = 'https://codesandbox.io/s/pie-chart-of-straight-angle-oz0th';
 
   render() {
+    const { data } = this.props;
     return (
       <ResponsiveContainer width="100%" height="200%">
         <PieChart width={400} height={400}>
