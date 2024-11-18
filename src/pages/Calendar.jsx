d3b714a851da8fe122be817bb0426311903c8104
iznalfiden <madiyar.iznalfiden@gmail.com>
2024-11-18 13:55:42 +0500

    add ts lint

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -96,7 +96,6 @@ const Scheduler = () => {
         );
     }
 
-    useEffect(() => {}, [currentShifts]);
     return (
         <div className="m-2 mb-0 p-2 pb-0  bg-white rounded-3xl">
             <Header category="Учёт" title="Смены" />
