eb3cb9c12e65d0a15053068a243cd109f5d5ca59
iznalfiden <madiyar.iznalfiden@gmail.com>
2024-11-18 15:58:09 +0500

    add modal outside close

--- src/components/Accounting/Workers/ProfileModal.jsx
+++ src/components/Accounting/Workers/ProfileModal.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { ProfileModalHeader } from './ProfileModalHeader';
 import { AnalyticsAccess } from './AnalyticsAccess';
 import { DataEditing } from './DataEditing';
 import { Button } from '@mantine/core';
 
 export const ProfileModal = ({ isOpen, onClose, user }) => {
+    const modalContentRef = useRef(null);
+
     if (!isOpen) return null;
 
+    const handleOverlayClick = (e) => {
+        if (modalContentRef.current && !modalContentRef.current.contains(e.target)) {
+            onClose();
+        }
+    };
+
     return (
-        <div className="flex flex-col fixed inset-0 bg-black bg-opacity-50 justify-center items-center z-50">
-            <div className="bg-white rounded-xl shadow-lg w-[430px] p-6 relative font-comfortaa flex flex-col justify-center gap-10">
+        <div
+            onClick={handleOverlayClick}
+            className="flex flex-col fixed inset-0 bg-black bg-opacity-50 justify-center items-center z-50"
+        >
+            <div
+                ref={modalContentRef}
+                className="bg-white rounded-xl shadow-lg w-[430px] p-6 relative font-comfortaa flex flex-col justify-center gap-10"
+            >
                 <ProfileModalHeader onClose={onClose} />
                 <AnalyticsAccess />
                 <DataEditing />
