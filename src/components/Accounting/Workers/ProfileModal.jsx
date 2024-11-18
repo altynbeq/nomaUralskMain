257b81e9b5d662fadafe15be5f1d2953c2d1527a
iznalfiden <madiyar.iznalfiden@gmail.com>
2024-11-18 15:55:05 +0500

    add profile modal

--- src/components/Accounting/Workers/ProfileModal.jsx
+++ src/components/Accounting/Workers/ProfileModal.jsx
@@ -9,7 +9,7 @@ export const ProfileModal = ({ isOpen, onClose, user }) => {
 
     return (
         <div className="flex flex-col fixed inset-0 bg-black bg-opacity-50 justify-center items-center z-50">
-            <div className="bg-white rounded-xl shadow-lg w-[400px] p-6 relative font-comfortaa flex flex-col justify-center gap-10">
+            <div className="bg-white rounded-xl shadow-lg w-[430px] p-6 relative font-comfortaa flex flex-col justify-center gap-10">
                 <ProfileModalHeader onClose={onClose} />
                 <AnalyticsAccess />
                 <DataEditing />
--- src/components/Accounting/Workers/ProfileModalHeader.tsx
+++ src/components/Accounting/Workers/ProfileModalHeader.tsx
@@ -11,7 +11,7 @@ export const ProfileModalHeader: FC<ProfileModalHeaderProps> = ({ onClose }) => 
         <h1 className="text-2xl font-bold">Доступ к департаменту</h1>
         <Button
             onClick={onClose}
-            className="absolute top-3 right-3 w-12 h-12 flex items-center justify-center bg-white  rounded-full"
+            className="absolute top-4 right-3 w-12 h-12 flex items-center justify-center bg-white  rounded-full"
         >
             <IoMdCloseCircle size={25} />
         </Button>
