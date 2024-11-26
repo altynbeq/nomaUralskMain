3049482b5da1e2a43d88c57945c2908994717a9e
altynbeq <starknbek@gmail.com>
2024-11-26 21:10:07 +0500

    storePlans calendar added

--- src/components/General/GeneralCompany.jsx
+++ src/components/General/GeneralCompany.jsx
@@ -4,6 +4,7 @@ import LoadingSkeleton from '../../components/LoadingSkeleton';
 import { PaidToAmount } from '../../components/Finance';
 import CardWithStats from '../../components/demo/ChartsHolder';
 import PeriodStats from '../../components/demo/PeriodStats';
+import { StoreSalesPlan } from '../General/StoreSalesPlan';
 
 function convertUrl(apiUrl) {
     return apiUrl.replace(/^http:\/\/\d{1,3}(\.\d{1,3}){3}:\d+\//, '/api/');
@@ -45,6 +46,9 @@ export const GeneralCompany = ({ urls }) => {
 
     return (
         <div className="mt-12 flex flex-col gap-6 align-center  justify-center">
+            <div className="flex mt-5 gap-4  w-[100%] flex-col md:flex-row  justify-center align-top      items-center">
+                <StoreSalesPlan />
+            </div>
             <div className="flex mt-5 gap-4  w-[100%] flex-col md:flex-row  justify-center align-top      items-center">
                 <CardWithStats userKkmUrl={userKkmUrl} userReceiptsUrl={userReceiptsUrl} />
             </div>
