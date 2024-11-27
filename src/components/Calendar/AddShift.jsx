54a1922ed8997865bcbf0bf3e649da4fa6f36607
iznalfiden <madiyar.iznalfiden@gmail.com>
2024-11-27 14:15:33 +0500

    save changes

--- src/components/Calendar/AddShift.jsx
+++ src/components/Calendar/AddShift.jsx
@@ -68,6 +68,7 @@ export const AddShift = (props) => {
                         subUserId: selectedSubuser._id,
                         startTime: start.toISOString(),
                         endTime: end.toISOString(),
+                        selectedStore: props.selectedStore,
                     }),
                 },
             );
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { CalendarModal } from '../CalendarModal';
-import { FaRegEdit } from 'react-icons/fa';
-import { FaCalendarWeek, FaCalendarAlt } from 'react-icons/fa';
 
 export const Profile = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [selectedDay, setSelectedDay] = useState(null);
     const [isMonthView, setIsMonthView] = useState(true);
+    const [subuserShifts, setSubuserShifts] = useState([]);
+    const [selectedShift, setSelectedShift] = useState(null);
 
     const toggleView = () => {
         setIsMonthView(!isMonthView);
@@ -15,19 +15,51 @@ export const Profile = () => {
 
     const openModal = (day) => {
         setSelectedDay(day);
+        const formattedDay = new Date(new Date().setDate(day)).toISOString().slice(0, 10);
+        const shift = subuserShifts.find((shift) => {
+            const startDate = shift.startTime.slice(0, 10);
+            const endDate = shift.endTime.slice(0, 10);
+            return startDate === formattedDay || endDate === formattedDay;
+        });
+
+        setSelectedShift(shift || null);
         setIsModalOpen(true);
     };
     const getDayColor = (day) => {
-        const dayData = dailyData.find((item) => item.date === day);
-        if (!dayData) return 'bg-gray-300';
+        // const dayData = dailyData.find((item) => item.date === day);
+        // if (!dayData) return 'bg-gray-300';
 
-        if (dayData.tasksCompleted && !dayData.wasLate) {
-            return 'bg-green-500';
-        } else if (!dayData.wasLate) {
-            return 'bg-yellow-400';
+        // if (dayData.tasksCompleted && !dayData.wasLate) {
+        //     return 'bg-green-500';
+        // } else if (!dayData.wasLate) {
+        //     return 'bg-yellow-400';
+        // } else {
+        //     return 'bg-red-500';
+        // }
+        const formattedDay = new Date(new Date().setDate(day)).toISOString().slice(0, 10);
+
+        const shift = subuserShifts.find((shift) => {
+            const startDate = shift.startTime.slice(0, 10); // Оставляем только дату
+            const endDate = shift.endTime.slice(0, 10); // Оставляем только дату
+            return startDate === formattedDay || endDate === formattedDay;
+        });
+        // Если найден подходящий сдвиг, выделяем оранжевым
+        if (shift) {
+            return 'bg-blue-500';
         } else {
-            return 'bg-red-500';
+            const dayData = dailyData.find((item) => item.date === day);
+            if (!dayData) return 'bg-gray-300';
+
+            if (dayData.tasksCompleted && !dayData.wasLate) {
+                return 'bg-green-500';
+            } else if (!dayData.wasLate) {
+                return 'bg-yellow-400';
+            } else {
+                return 'bg-red-500';
+            }
         }
+
+        // return 'bg-gray-300';
     };
     const dailyData = [
         {
@@ -63,12 +95,33 @@ export const Profile = () => {
         },
     ];
     const getProgressWidth = () => {
-        // if (!dayData) return '0%';
-
-        const planAmount = '600 000';
-        const actualAmount = '100 000';
         return `60%`;
     };
+
+    useEffect(() => {
+        const subuserId = localStorage.getItem('_id');
+        const fetchSubUserShifts = async () => {
+            try {
+                const response = await fetch(
+                    `https://nomalytica-back.onrender.com/api/shifts/${subuserId}`,
+                    {
+                        method: 'GET',
+                        headers: { 'Content-Type': 'application/json' },
+                    },
+                );
+
+                if (!response.ok) {
+                    throw new Error(`Error: ${response.status} - ${response.statusText}`);
+                }
+
+                const result = await response.json();
+                setSubuserShifts(result);
+            } catch (error) {
+                console.error('Error fetching sub-user data:', error);
+            }
+        };
+        fetchSubUserShifts();
+    }, []);
 
     return (
         <div className="w-[90%] md:w-[50%] ml-5  bg-white subtle-border rounded-lg shadow-md p-4 ">
@@ -171,109 +224,8 @@ export const Profile = () => {
                 onClose={() => setIsModalOpen(false)}
                 selectedDay={selectedDay}
                 dailyData={dailyData}
+                selectedShift={selectedShift}
             />
         </div>
     );
 };
-// import { useState } from 'react';
-// import { CalendarModal } from '../CalendarModal';
-// import { useStateContext } from '../../contexts/ContextProvider';
-
-// export const Profile = () => {
-//     const [isModalOpen, setIsModalOpen] = useState(false);
-//     const [selectedDay, setSelectedDay] = useState(null);
-
-//     const { subUser } = useStateContext();
-
-//     const dailyData = [
-//         {
-//             date: 1,
-//             tasksCompleted: true,
-//             wasLate: false,
-//             workHours: '12:00 - 20:00',
-//             cameAt: '12:00',
-//             leftAt: '20:00',
-//             plan: '600 000 тг',
-//             actual: '600 000 тг',
-//         },
-//         {
-//             date: 2,
-//             tasksCompleted: false,
-//             wasLate: true,
-//             workHours: '12:00 - 20:00',
-//             cameAt: '12:30',
-//             leftAt: '19:00',
-//             plan: '600 000 тг',
-//             actual: '480 000 тг',
-//         },
-
-//         {
-//             date: 3,
-//             tasksCompleted: false,
-//             wasLate: false,
-//             workHours: '12:00 - 20:00',
-//             cameAt: '12:30',
-//             leftAt: '19:00',
-//             plan: '600 000 тг',
-//             actual: '480 000 тг',
-//         },
-//     ];
-
-//     const openModal = (day) => {
-//         setSelectedDay(day);
-//         setIsModalOpen(true);
-//     };
-
-//     const getDayColor = (day) => {
-//         const dayData = dailyData.find((item) => item.date === day);
-//         if (!dayData) return 'bg-gray-300';
-
-//         if (dayData.tasksCompleted && !dayData.wasLate) {
-//             return 'bg-green-500';
-//         } else if (!dayData.wasLate) {
-//             return 'bg-yellow-400';
-//         } else {
-//             return 'bg-red-500';
-//         }
-//     };
-
-//     return (
-//         <div className="max-w-xs mx-auto bg-white rounded-lg shadow-md p-4 mt-[10%]">
-//             <div className="flex items-center mb-4">
-//                 <div className="w-12 h-12 bg-gray-300 rounded-full mr-4"></div>
-//                 <div>
-//                     <h2 className="font-semibold text-lg text-black">{subUser?.name}</h2>
-//                     <p className="text-gray-500 text-sm">Сотрудник</p>
-//                 </div>
-//             </div>
-//             <hr className="bg-red max-w-xs mx-auto my-4" />
-//             <div className="flex justify-between mb-4">
-//                 <button className="w-1/2 bg-blue-500 text-white py-2 rounded-md mr-2">
-//                     Списание
-//                 </button>
-//                 <button className="w-1/2 bg-blue-500 text-white py-2 rounded-md ml-2">Смена</button>
-//             </div>
-
-//             <div className="grid grid-cols-7 gap-2">
-//                 {[...Array(31).keys()].map((day) => (
-//                     <button
-//                         key={day + 1}
-//                         onClick={() => openModal(day + 1)}
-//                         className={`w-8 h-8 flex items-center justify-center rounded-full ${getDayColor(
-//                             day + 1,
-//                         )} border text-sm`}
-//                     >
-//                         {day + 1}
-//                     </button>
-//                 ))}
-//             </div>
-
-//             <CalendarModal
-//                 isOpen={isModalOpen}
-//                 onClose={() => setIsModalOpen(false)}
-//                 selectedDay={selectedDay}
-//                 dailyData={dailyData}
-//             />
-//         </div>
-//     );
-// };
