05beab7a6f3caff182a556079f948ba7b98c4be7
iznalfiden <madiyar.iznalfiden@gmail.com>
2024-12-11 22:37:17 +0500

    fix

--- src/components/Accounting/Workers/EmployeeCalendar.jsx
+++ src/components/Accounting/Workers/EmployeeCalendar.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useMemo, useCallback, useEffect } from 'react';
-import { Dropdown } from 'primereact/dropdown';
 import { InputText } from 'primereact/inputtext';
 import { formatOnlyTimeDate } from '../../../methods/dataFormatter';
 import { FaSearch, FaPlus, FaFilter } from 'react-icons/fa';
@@ -11,6 +10,7 @@ import { socket } from '../../../socket'; // путь к вашему socket.js
 import { PaginationControls } from '../PaginationControls';
 import { ShiftModal } from './ShiftModal';
 import { CalendarTable } from './CalendarTable';
+import { Filters } from './Filters';
 
 export const EmployeeCalendar = () => {
     const stores = useCompanyStructureStore((state) => state.stores);
@@ -505,39 +505,16 @@ export const EmployeeCalendar = () => {
                                 onShiftsAdded={handleShiftsAdded}
                             />
                         </div>
-                        <div className="relative">
-                            <button
-                                className="bg-blue-500 text-white flex items-center gap-2 px-4 py-2 rounded-2xl hover:bg-blue-600 focus:ring-2 focus:ring-blue-300"
-                                onClick={() => setIsFilterOpen(!isFilterOpen)}
-                            >
-                                <FaFilter />
-                                <span>Фильтр</span>
-                            </button>
-
-                            {isFilterOpen && (
-                                <div className="absolute z-10 bg-white p-4 mt-2 w-72 shadow-lg rounded-lg border border-gray-200">
-                                    <Dropdown
-                                        value={selectedStore}
-                                        onChange={(e) => setSelectedStore(e.value)}
-                                        showClear
-                                        options={stores || []}
-                                        optionLabel="storeName"
-                                        placeholder="Магазин"
-                                        className="w-full mb-3 border-blue-500 border-2 text-black rounded-lg focus:ring-2 focus:ring-blue-300"
-                                    />
-
-                                    <Dropdown
-                                        value={selectedDepartment}
-                                        onChange={(e) => setSelectedDepartment(e.value)}
-                                        options={filteredDepartments || []}
-                                        optionLabel="name"
-                                        placeholder="Отдел"
-                                        disabled={!selectedStore}
-                                        className="w-full border-blue-500 border-2 text-black rounded-lg focus:ring-2 focus:ring-blue-300"
-                                    />
-                                </div>
-                            )}
-                        </div>
+                        <Filters
+                            isFilterOpen={isFilterOpen}
+                            setIsFilterOpen={setIsFilterOpen}
+                            selectedStore={selectedStore}
+                            setSelectedStore={setSelectedStore}
+                            selectedDepartment={selectedDepartment}
+                            setSelectedDepartment={setSelectedDepartment}
+                            stores={stores}
+                            filteredDepartments={filteredDepartments}
+                        />
                         <div className="relative">
                             <InputText
                                 value={searchTerm}
