324eb5b560007535c8496467816fd7082c2a28c4
iznalfiden <madiyar.iznalfiden@gmail.com>
2025-01-03 16:59:37 +0500

    save

--- src/components/Accounting/Warehouse/Products.jsx
+++ src/components/Accounting/Warehouse/Products.jsx
@@ -6,7 +6,7 @@ import { InputText } from 'primereact/inputtext';
 import { EditProductModal } from '../../EditProductModal';
 import { useCompanyStore } from '../../../store';
 import { MdEdit } from 'react-icons/md';
-import { FaFilter, FaTimes, FaExclamationTriangle, FaAngleDown, FaAngleUp } from 'react-icons/fa';
+import { FaFilter, FaTimes, FaExclamationTriangle } from 'react-icons/fa';
 
 export function Products({ title }) {
     const warehouses = useCompanyStore((state) => state.warehouses);
@@ -17,7 +17,6 @@ export function Products({ title }) {
     const [productSearch, setProductSearch] = useState('');
     const [selectedItems, setSelectedItems] = useState([]);
     const [showFilters, setShowFilters] = useState(false);
-    const [isCollapsed, setIsCollapsed] = useState(true);
     const filterRef = useRef(null);
 
     const getWarehouseNameFromProduct = (productName) => {
@@ -127,12 +126,6 @@ export function Products({ title }) {
                             <FaFilter className="h-5 w-5 text-gray-600" />
                             <span>Фильтр</span>
                         </button>
-                        <div
-                            className="mr-4 cursor-pointer text-2xl"
-                            onClick={() => setIsCollapsed(!isCollapsed)}
-                        >
-                            {isCollapsed ? <FaAngleDown /> : <FaAngleUp />}
-                        </div>
                     </div>
                 </div>
                 {showFilters && (
@@ -163,52 +156,51 @@ export function Products({ title }) {
                         </button>
                     </div>
                 )}
-                {!isCollapsed && (
-                    <div className="flex flex-wrap border-solid border-1 rounded-xl px-2 gap-1 w-full">
-                        <DataGrid
-                            autoHeight
-                            checkboxSelection
-                            rows={rows}
-                            columns={columns}
-                            getRowClassName={(params) =>
-                                params.indexRelativeToCurrentPage % 2 === 0 ? 'even' : 'odd'
-                            }
-                            initialState={{
-                                pagination: { paginationModel: { pageSize: 20 } },
-                            }}
-                            pageSizeOptions={[10, 20, 50]}
-                            disableColumnResize
-                            onRowSelectionModelChange={handleSelectionChange}
-                            density="compact"
-                            slotProps={{
-                                filterPanel: {
-                                    filterFormProps: {
-                                        logicOperatorInputProps: {
+
+                <div className="flex flex-wrap border-solid border-1 rounded-xl px-2 gap-1 w-full">
+                    <DataGrid
+                        autoHeight
+                        checkboxSelection
+                        rows={rows}
+                        columns={columns}
+                        getRowClassName={(params) =>
+                            params.indexRelativeToCurrentPage % 2 === 0 ? 'even' : 'odd'
+                        }
+                        initialState={{
+                            pagination: { paginationModel: { pageSize: 20 } },
+                        }}
+                        pageSizeOptions={[10, 20, 50]}
+                        disableColumnResize
+                        onRowSelectionModelChange={handleSelectionChange}
+                        density="compact"
+                        slotProps={{
+                            filterPanel: {
+                                filterFormProps: {
+                                    logicOperatorInputProps: {
+                                        variant: 'outlined',
+                                        size: 'small',
+                                    },
+                                    columnInputProps: {
+                                        variant: 'outlined',
+                                        size: 'small',
+                                        sx: { mt: 'auto' },
+                                    },
+                                    operatorInputProps: {
+                                        variant: 'outlined',
+                                        size: 'small',
+                                        sx: { mt: 'auto' },
+                                    },
+                                    valueInputProps: {
+                                        InputComponentProps: {
                                             variant: 'outlined',
                                             size: 'small',
-                                        },
-                                        columnInputProps: {
-                                            variant: 'outlined',
-                                            size: 'small',
-                                            sx: { mt: 'auto' },
-                                        },
-                                        operatorInputProps: {
-                                            variant: 'outlined',
-                                            size: 'small',
-                                            sx: { mt: 'auto' },
-                                        },
-                                        valueInputProps: {
-                                            InputComponentProps: {
-                                                variant: 'outlined',
-                                                size: 'small',
-                                            },
                                         },
                                     },
                                 },
-                            }}
-                        />
-                    </div>
-                )}
+                            },
+                        }}
+                    />
+                </div>
             </div>
         </div>
     );
